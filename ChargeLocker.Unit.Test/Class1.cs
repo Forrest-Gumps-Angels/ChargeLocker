15b089718a45adf9be919517ece304a97fba5061
Viktor Lundsgaard Andersen <vik.lund.and@gmail.com>
2020-02-26 15:55:59 +0100

    Adde NuGet packages

--- ChargeLocker.Unit.Test/Class1.cs
+++ ChargeLocker.Unit.Test/Class1.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace ChargeLocker.Unit.Test
 {
-    public class Class1
+    [TestFixture]
+    public class ECSUnitTest
     {
+        private ECS _uut;
+        private IHeater _heater;
+        private ITempSensor _sensor;
+        private IWindow _window;
+        private int _lowerTemperatureThreshold = -50;
+        private int _upperTemperatureThreshold = 50;
+
+        [SetUp]
+        public void Setup()
+        {
+            _heater = Substitute.For<IHeater>();
+            _sensor = Substitute.For<ITempSensor>();
+            _window = Substitute.For<IWindow>();
+
+            _uut = new ECS(_sensor, _heater, _window, _lowerTemperatureThreshold, _upperTemperatureThreshold);
+        }
+
+
+        [Test]
+        public void GetTemp_TempLowerThanThreshold()
+        {
+            _sensor.GetTemp().Returns(-51);
+            _uut.Regulate();
+
+            _heater.Received().TurnOn();
+            _window.Received().Close();
+        }
     }
-}
