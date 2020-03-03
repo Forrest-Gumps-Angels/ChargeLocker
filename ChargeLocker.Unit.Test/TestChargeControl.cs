6f8382bcf4682fa1b7a94aac1f876da1bd3a9b2a
Viktor Lundsgaard Andersen <vik.lund.and@gmail.com>
2020-03-03 13:41:17 +0100

    Added interfaces

--- ChargeLocker.Unit.Test/TestChargeControl.cs
+++ ChargeLocker.Unit.Test/TestChargeControl.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UsbSimulator;
 
 namespace ChargeLocker.Unit.Test
 {
-    class TestChargeControl
+    [TestFixture]
+    public class TestUsbChargerSimulator
     {
+        private UsbChargerSimulator _uut;
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new StationControl();
+        }
+
+        [Test]
+        public void ctor_IsConnected()
+        {
+            Assert.That(_uut.Connected, Is.True);
+        }
+
+        [Test]
+        public void ctor_CurentValueIsZero()
+        {
+            Assert.That(_uut.CurrentValue, Is.Zero);
+        }
     }
 }
