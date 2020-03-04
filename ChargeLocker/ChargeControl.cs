ff4b180c4d7d85face1de32b693c32f4c413da40
Rasmus <201806493@post.au.dk>
2020-03-04 15:44:48 +0100

    ChargeControl done

--- ChargeLocker/ChargeControl.cs
+++ ChargeLocker/ChargeControl.cs
@@ -9,19 +9,44 @@ namespace ChargeLocker
 {
     public class ChargeControl : IChargeControl
     {
+        private IUsbCharger _charger;
         private double _current;
+
+        public ChargeControl(IUsbCharger charger)
+        {
+            _charger = charger;
+        }
 
         public bool IsConnected()
         {
-         switch   
+            switch (_current)
+            {
+                case 0:
+                        return false;
+                    
+                case double current when (0.0 < current && current <= 5):
+                    return true;
+
+                case double current when (5.0 < current && current <= 500):
+                    return true;
+
+                case double current when (current > 500):
+                    return true;
+
+                default:
+                    return false;
+
+                    // Simple if statement would do...
+            }
+            
         }
         public void StartCharge()
-        { 
-
+        {
+            _charger.StartCharge();
         }
         public void StopCharge()
-        { 
-
+        {
+            _charger.StopCharge();
         }
         public void currentChangedEventHandler(object sender, CurrentChangedEventArgs e)
         {
