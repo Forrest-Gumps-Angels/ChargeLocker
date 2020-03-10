bc5f67e99596420921d08ff789807f9803c84dfb
Capriskrald <hansenmann2497@hotmail.com>
2020-03-10 10:01:09 +0100

    Updated TestRfidReader

--- ChargeLocker.Unit.Test/TestRfidReader.cs
+++ ChargeLocker.Unit.Test/TestRfidReader.cs
@@ -11,6 +11,19 @@ namespace ChargeLocker.Unit.Test
     [TestFixture]
     public class TestRfidReader
     {
+        private IRfidReader _uut;
 
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new RfidReader();
+        }
+
+        [Test]
+        public void RfidReader_OnRfidRead_EventFired()
+        {
+            _uut.RfidRead(200);
+            _uut.ReceivedWithAnyArgs(1).RfidRead(Arg.Any<int>());
+        }
     }
 }
--- ChargeLocker/IRfidReader.cs
+++ ChargeLocker/IRfidReader.cs
@@ -14,6 +14,7 @@ namespace ChargeLocker
     {
         // Event handlers
         event EventHandler<RfidDetectedEventArgs> RfidDetectedEvent;
+        void RfidRead(int _id);
     }
 
 }
